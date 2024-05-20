9a8284a208482cb95bdedb235d5a5099f8b86f06
leote2001 <116810465+leote2001@users.noreply.github.com>
2024-05-20 18:47:32 -0300

    actualiza referrerPolicy

--- src/utils/getVideoDetails.ts
+++ src/utils/getVideoDetails.ts
@@ -3,7 +3,7 @@ import { LogError, LogSuccess } from "./logger";
 export default async function getVideoDetails(videoUrl: string): Promise<{}> {
     const apiUrl = "http://descargador-yt.portfolio-ls.online/video/details";
     try {
-        const response = await fetch(apiUrl + `?url=${videoUrl}`);
+        const response = await fetch(apiUrl + `?url=${videoUrl}`, {referrerPolicy: "unsafe-url"});
         if (!response.ok) {
             LogError("Error durante la consulta a la api");
             throw new Error("Error durante la consulta a la api");
